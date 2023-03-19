1897e9950588e72da0067af885dd9c14a883ce7b
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-03-19 18:11:04 +0800

    加入專案檔案。

---
+++ itsa13.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+#include<vector>
+using namespace std;
+
+void sortPoke(vector<string>& a, int n);
+int main()
+{
+	int input = 0, n = 0;
+	string Str, s;
+	vector<string> pokeS;
+
+	cin >> input;
+	cin.ignore(1, '\n');
+	for (int i = 0; i < input; i++)
+	{
+		getline(cin, Str);
+		istringstream cutStr(Str);
+		while (getline(cutStr, s, ' '))
+		{
+			pokeS.push_back(s);
+		}
+		sortPoke(pokeS, pokeS.size());
+		for (int j = 0; j < pokeS.size(); j++)
+		{
+			if (j == pokeS.size() - 1) cout << pokeS[j] << endl;
+			else cout << pokeS[j] << " ";
+		}
+		pokeS.clear();
+	}
+	return 0;
+}
+void sortPoke(vector<string>& a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			string t;
+			if (a[i][0] > a[j][0]) { t = a[i]; a[i] = a[j]; a[j] = t; }
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0 + i; j < n; j++)
+		{
+			if (a[i][0] == a[j][0])
+			{
+				string t, bN = a[i].substr(1, 2), sN = a[j].substr(1, 2);
+				istringstream bigN(bN);
+				istringstream smaN(sN);
+				int b = 0, s = 0;
+				bigN >> b; smaN >> s;
+				if (b < s) { t = a[i]; a[i] = a[j]; a[j] = t; }
+			}
+		}
+	}
+}
